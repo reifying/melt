a727405bde8d3966734d4621bec86528e5e13f7c
Travis Brown <travis.brown@mckesson.com>
2019-01-05 07:03:18 -0600

    extract config into its own file

--- melt/analyze.clj
+++ melt/analyze.clj
@@ -2,27 +2,8 @@
   (:require [clojure.data :as data]
             [clojure.java.io :as io]
             [clojure.java.jdbc :as jdbc]
-            [clojure.pprint :refer [pprint]]))
-
-(def mssql-host   (System/getenv "TEST_MSSQL_HOST"))
-(def mssql-port   (or (System/getenv "TEST_MSSQL_PORT") "1433"))
-(def mssql-user   (System/getenv "TEST_MSSQL_USER"))
-(def mssql-pass   (System/getenv "TEST_MSSQL_PASS"))
-(def mssql-dbname (System/getenv "TEST_MSSQL_NAME"))
-(def jtds-host    (or (System/getenv "TEST_JTDS_HOST") mssql-host))
-(def jtds-port    (or (System/getenv "TEST_JTDS_PORT") mssql-port))
-(def jtds-user    (or (System/getenv "TEST_JTDS_USER") mssql-user))
-(def jtds-pass    (or (System/getenv "TEST_JTDS_PASS") mssql-pass))
-(def jtds-dbname  (or (System/getenv "TEST_JTDS_NAME") mssql-dbname))
-
-(def db {:dbtype   "jtds"
-         :dbname   jtds-dbname
-         :host     jtds-host
-         :port     jtds-port
-         :user     jtds-user
-         :password jtds-pass})
-
-(def ignorable-schemas #{"sys" "INFORMATION_SCHEMA"})
+            [clojure.pprint :refer [pprint]]
+            [melt.config :refer [db ignorable-schemas]]))
 
 (defn user-schema? [{:keys [table_schem]}]
   (not (contains? ignorable-schemas table_schem)))
---
+++ melt/config.clj
@@ -0,0 +1,21 @@
+(ns melt.config)
+
+(def mssql-host   (System/getenv "TEST_MSSQL_HOST"))
+(def mssql-port   (or (System/getenv "TEST_MSSQL_PORT") "1433"))
+(def mssql-user   (System/getenv "TEST_MSSQL_USER"))
+(def mssql-pass   (System/getenv "TEST_MSSQL_PASS"))
+(def mssql-dbname (System/getenv "TEST_MSSQL_NAME"))
+(def jtds-host    (or (System/getenv "TEST_JTDS_HOST") mssql-host))
+(def jtds-port    (or (System/getenv "TEST_JTDS_PORT") mssql-port))
+(def jtds-user    (or (System/getenv "TEST_JTDS_USER") mssql-user))
+(def jtds-pass    (or (System/getenv "TEST_JTDS_PASS") mssql-pass))
+(def jtds-dbname  (or (System/getenv "TEST_JTDS_NAME") mssql-dbname))
+
+(def db {:dbtype   "jtds"
+         :dbname   jtds-dbname
+         :host     jtds-host
+         :port     jtds-port
+         :user     jtds-user
+         :password jtds-pass})
+
+(def ignorable-schemas #{"sys" "INFORMATION_SCHEMA"})
