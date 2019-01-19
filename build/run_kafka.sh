91a985731cc6d059892f9c85570b1efa74dc9d7f
Travis Brown <travis@unt.edu>
2019-01-19 10:47:49 -0600

    Create topics

--- build/run_kafka.sh
+++ build/run_kafka.sh
@@ -5,5 +5,6 @@ docker pull wurstmeister/kafka
 git clone https://github.com/wurstmeister/kafka-docker.git
 cd kafka-docker
 sed -i "s/KAFKA_ADVERTISED_HOST_NAME:.*/KAFKA_ADVERTISED_HOST_NAME: `hostname`/" docker-compose.yml
-grep KAFKA_ADVERTISED_HOST_NAME docker-compose.yml
+sed -i "s/kafka:/kafka:\n    container_name: kafka-broker/" docker-compose.yml
+cat docker-compose.yml
 docker-compose up -d
