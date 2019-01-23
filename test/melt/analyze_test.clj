8866da65a32f377facb242971eda6552a0a4c186
Travis Brown <travis@unt.edu>
2019-01-22 22:11:17 -0600

    Stay sane

--- test/melt/analyze_test.clj
+++ test/melt/analyze_test.clj
@@ -1,5 +1,7 @@
 (ns melt.analyze-test
-  (:require [midje.sweet :refer :all]
+  (:require [clj-time.coerce :refer [to-sql-time]]
+            [clj-time.local :as l]
+            [midje.sweet :refer [fact =>]]
             [melt.analyze :as a]))
 
 (fact "`schema` reads a minimal amount of info describing the DB schema"
@@ -21,4 +23,4 @@
        :addressid    638
        :addresstype  "Main Office"
        :rowguid      "ACDA2178-9538-46D6-B023-01EB79BF7872"
-       :modifieddate (java.sql.Timestamp. 1185944400000)})
+       :modifieddate (to-sql-time (l/to-local-date-time "2007-08-01"))})
