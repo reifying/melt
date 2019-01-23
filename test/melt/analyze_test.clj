50bccc112dae5fc4b48f00f3e4a513ac58a8701c
Travis Brown <travis@unt.edu>
2019-01-22 21:48:21 -0600

    Test read-table into map

--- test/melt/analyze_test.clj
+++ test/melt/analyze_test.clj
@@ -1,11 +1,24 @@
 (ns melt.analyze-test
-  (:require [clojure.test :as t]
+  (:require [midje.sweet :refer :all]
             [melt.analyze :as a]))
 
-(t/deftest reading-schema
-  (t/testing "it reads a minimal amount of info describing the DB schema"
-    (t/is (= (get (a/schema) {:name   "CustomerAddress"
-                              :cat    "AdventureWorks"
-                              :schema "SalesLT"})
-             {:columns ["ModifiedDate" "rowguid" "AddressType" "AddressID" "CustomerID"]
-              :keys    ["CustomerID" "AddressID"]}))))
+(fact "`schema` reads a minimal amount of info describing the DB schema"
+      (get (a/schema) {:name   "CustomerAddress"
+                       :cat    "AdventureWorks"
+                       :schema "SalesLT"}) =>
+      {:columns ["ModifiedDate" "rowguid" "AddressType" "AddressID" "CustomerID"]
+       :keys    [:customerid :addressid]})
+
+(fact "`read-table` reads all the rows of a table and creates a map on primary keys"
+      (get (a/read-table [{:name   "CustomerAddress"
+                           :cat    "AdventureWorks"
+                           :schema "SalesLT"}
+                          {:keys [:customerid :addressid]}])
+           {:customerid 29926
+            :addressid  638})
+      =>
+      {:customerid   29926
+       :addressid    638
+       :addresstype  "Main Office"
+       :rowguid      "ACDA2178-9538-46D6-B023-01EB79BF7872"
+       :modifieddate (java.sql.Timestamp. 1185944400000)})
---
+++ test/melt/test_runner.clj
@@ -0,0 +1,9 @@
+(ns melt.test-runner
+  (:require [midje.repl :refer [load-facts]]))
+
+(defn -main
+  "Return non-zero exit code if a test failure is found"
+  []
+  (let [results (load-facts)]
+    (if (< 0 (:failures results))
+      (System/exit 1))))
