0e7b39743aaae098c23ef914d517e4a2fe992cc2
Travis Brown <travis@unt.edu>
2019-01-31 07:19:30 -0600

    Expose kafka on port 9092

--- build/run_kafka.sh
+++ build/run_kafka.sh
@@ -5,6 +5,9 @@ docker pull wurstmeister/kafka
 git clone https://github.com/wurstmeister/kafka-docker.git
 cd kafka-docker
 sed -i "s/KAFKA_ADVERTISED_HOST_NAME:.*/KAFKA_ADVERTISED_HOST_NAME: `hostname`/" docker-compose.yml
-sed -i "s/kafka:/kafka:\n    container_name: kafka-broker/" docker-compose.yml
+sed -i "s/kafka:/kafka:\\n    container_name: kafka-broker/" docker-compose.yml
+sed -i "s|image: wurstmeister/zookeeper|image: wurstmeister/zookeeper\\n    container_name: zookeeper|" docker-compose.yml
+sed -i 's/"9092"/"9092:9092"/' docker-compose.yml
 cat docker-compose.yml
 docker-compose up -d
+
