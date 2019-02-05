89c5b15456277effec96aaa9e1b2ef6bf9a2a404
Travis Brown <travis@unt.edu>
2019-02-05 06:53:11 -0600

    Use advertised host name for kafka as 127.0.0.1

--- build/run_kafka.sh
+++ build/run_kafka.sh
@@ -4,7 +4,7 @@ set -e
 docker pull wurstmeister/kafka
 git clone https://github.com/wurstmeister/kafka-docker.git
 cd kafka-docker
-sed -i "s/KAFKA_ADVERTISED_HOST_NAME:.*/KAFKA_ADVERTISED_HOST_NAME: `hostname`/" docker-compose.yml
+sed -i "s/KAFKA_ADVERTISED_HOST_NAME:.*/KAFKA_ADVERTISED_HOST_NAME: 127.0.0.1/" docker-compose.yml
 sed -i "s/kafka:/kafka:\\n    container_name: kafka-broker/" docker-compose.yml
 sed -i "s|image: wurstmeister/zookeeper|image: wurstmeister/zookeeper\\n    container_name: zookeeper|" docker-compose.yml
 sed -i 's/"9092"/"9092:9092"/' docker-compose.yml
