3d3d6cab6036716de2a6a0d8be82161a1b28fbef
Travis Brown <travis@unt.edu>
2019-02-12 14:33:03 -0600

    Include all change tracking fields

--- src/melt/change_tracking.clj
+++ src/melt/change_tracking.clj
@@ -3,20 +3,12 @@
             [melt.channel :as ch]
             [melt.config :refer [db]]))
 
-(defn- select-columns [table]
-  (->> (::ch/keys table)
-       (map name)
-       (cons "sys_change_operation")
-       (map #(str "ct." %))
-       (String/join ", ")))
-
 (defn- qualified-table-name [table]
   (str (::ch/schema table) "." (::ch/name table)))
 
 (defn- change-sql [table]
   (String/join " "
-               ["Select"
-                (select-columns table)
+               ["Select ct.*"
                 "From CHANGETABLE(CHANGES "
                 (qualified-table-name table)
                 ", ?) As ct"]))
