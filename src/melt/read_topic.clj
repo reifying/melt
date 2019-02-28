504adbd3c231c423f49dfb7610f0e9aacfd275a4
Travis Brown <travis@unt.edu>
2019-02-27 22:46:52 -0600

    generalize consumer-seq
    Support REST API with continuous consumption in background thread.

--- src/melt/read_topic.clj
+++ src/melt/read_topic.clj
@@ -39,18 +39,18 @@
     (<= end-offset (inc committed-offset))
     false))
 
-(defn fully-consumed? [consumed-offsets end-offsets]
-  (println "consumed? consumed:" consumed-offsets "end:" end-offsets)
-  (every? (partial at-end? consumed-offsets) end-offsets))
-
 (defn end-offsets [^Consumer c]
   (.endOffsets c (.assignment c)))
+
+(defn fully-consumed-fn [^Consumer c]
+  (let [end-offsets (end-offsets c)]
+    (fn [consumed-offsets]
+      (every? (partial at-end? consumed-offsets) end-offsets))))
 
 (defn assoc-offset [offsets message]
   (assoc offsets (:partition message) (:offset message)))
 
 (defn- poll [c]
-  (println "Polling")
   (map record (.poll c 1000)))
 
 (defn- seq-entry [record offsets]
@@ -58,25 +58,21 @@
    :offsets         offsets})
 
 (defn consumer-seq
-  ([^Consumer c]
-   (consumer-seq c {}))
   ([^Consumer c current-offsets]
-   (consumer-seq c current-offsets (end-offsets c)))
-  ([^Consumer c current-offsets end-offsets]
-   (consumer-seq c current-offsets end-offsets (poll c)))
-  ([^Consumer c current-offsets end-offsets messages]
+   (consumer-seq c current-offsets (fully-consumed-fn c) []))
+  ([^Consumer c current-offsets stop-consuming-fn messages]
    (lazy-seq
     (if (seq messages)
       (let [record      (first messages)
             cur-offsets (assoc-offset current-offsets record)]
         (cons (seq-entry record cur-offsets)
-              (consumer-seq c cur-offsets end-offsets (rest messages))))
-      (if-not (fully-consumed? current-offsets end-offsets)
-        (consumer-seq c current-offsets end-offsets (poll c)))))))
+              (consumer-seq c cur-offsets stop-consuming-fn (rest messages))))
+      (if-not (stop-consuming-fn current-offsets)
+        (consumer-seq c current-offsets stop-consuming-fn (poll c)))))))
 
 (defn count-topic [consumer-props topic]
   (with-open [c (consumer-at-beginning consumer-props [topic])]
-    (count (consumer-seq c))))
+    (count (consumer-seq c {}))))
 
 (defn- merge-seq-entry [topic-data seq-entry]
   (let [{:keys [topic key value]} (:consumer-record seq-entry)]
@@ -86,6 +82,21 @@
 
 (defn reduce-consumer-seq [c topic-data]
   (reduce merge-seq-entry topic-data (consumer-seq c (:offsets topic-data))))
+
+(defn- background-consume-fn [consumer-props topics reduced-atom shutdown-atom]
+  (fn []
+    (with-open [c (consumer-at-beginning consumer-props topics)]
+      (doseq [entry (consumer-seq c {} (fn [_] @shutdown-atom) [])]
+        (swap! reduced-atom (fn [topic-data]
+                              (merge-seq-entry topic-data entry)))))))
+
+(defn background-consume [consumer-props topics reduced-atom]
+  (let [shutdown (atom false)]
+    (.start
+     (Thread.
+      (background-consume-fn consumer-props topics reduced-atom shutdown)))
+    (reify java.lang.AutoCloseable
+      (close [this] (reset! shutdown true)))))
 
 (defn read-topics-loop [consumer-props topics retries]
   (with-open [c (consumer-at-beginning consumer-props topics)]
