a018a58a00fac0cc6d9870a630fbc95223b1f7a6
Travis Brown <4742936+reifying@users.noreply.github.com>
2019-03-18 15:27:27 -0500

    track-all should not include already tracked

--- src/melt/change_tracking.clj
+++ src/melt/change_tracking.clj
@@ -1,5 +1,6 @@
 (ns melt.change-tracking
   (:require [clojure.java.jdbc :as jdbc]
+            [clojure.set :refer [difference]]
             [melt.channel :as ch]
             [melt.config :refer [db]]
             [melt.jdbc :as mdb]))
@@ -15,12 +16,12 @@
 (defn qualified-table-name [table]
   (str (::ch/schema table) "." (::ch/name table)))
 
-(defn- track-table-sql [table]
+(defn track-table-sql [table]
   (str "ALTER TABLE " (qualified-table-name table) "
         ENABLE CHANGE_TRACKING
         WITH (TRACK_COLUMNS_UPDATED = OFF)"))
 
-(defn- untrack-table-sql [table]
+(defn untrack-table-sql [table]
   (str "ALTER TABLE " (qualified-table-name table) "
         DISABLE CHANGE_TRACKING"))
 
@@ -36,24 +37,29 @@
   ([] (trackable-tables (mdb/cached-schema)))
   ([schema] (filter #(seq (::ch/keys %)) schema)))
 
-(defn track-all []
-  (doall (map track-table (trackable-tables))))
-
-(defn- list-tracked []
+(defn list-tracked []
   (map (juxt :schema_name :table_name)
        (jdbc/query db
                    ["Select object_schema_name(object_id) schema_name, 
                             object_name(object_id) table_name 
                      From sys.change_tracking_tables"])))
 
-(defn tracked []
+(defn tracked [schema]
   (let [m (reduce #(assoc %1 ((juxt ::ch/schema ::ch/name) %2) %2)
                   {}
-                  (mdb/cached-schema))]
+                  schema)]
     (vals (select-keys m (list-tracked)))))
 
-(defn untrack-all []
-  (doseq [table (tracked)] (untrack-table table)))
+(defn trackable-untracked [schema]
+  (let [trackable (trackable-tables schema)
+        tracked   (tracked schema)]
+    (difference (set trackable) (set tracked))))
+
+(defn track-all [schema]
+  (doall (map track-table (trackable-untracked schema))))
+
+(defn untrack-all [schema]
+  (doseq [table (tracked schema)] (untrack-table table)))
 
 (defn change-sql [table]
   (String/join " "
