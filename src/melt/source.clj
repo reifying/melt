5ab3b2325c63de806ef66a3cc12ec4f60f4aa4e8
Travis Brown <4742936+reifying@users.noreply.github.com>
2019-03-28 07:58:56 -0500

    Remove commented

--- src/melt/source.clj
+++ src/melt/source.clj
@@ -13,18 +13,3 @@
     {::value row
      ::key    (select-keys row keys)}
     {::value row}))
-
-; (defn transduce-source [f xform val db source]
-;   (transduce (comp xform (map (partial message source)))
-;              f
-;              val
-;              (mdb/reducible-source db source)))
-
-; (defn reduce-source [f val db source]
-;   (transduce-source (completing f) (map (xform source)) val db source))
-
-; (defn- merge-by-key [acc row]
-;   (assoc acc (get row ::key) (get (s/assert ::keyed row) ::message)))
-
-; (defn source-by-key [db source]
-;   (reduce-source merge-by-key {} db source))
